81666d2bdd25862fdae7cc191300c4bd5af269e5
Rahul <rahulsingh2240@gmail.com>
2018-06-07 13:22:00 +0530

    Update the doc

--- playground/mongo-update.js
+++ playground/mongo-update.js
@@ -8,14 +8,17 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   var db = client.db('TodoApp');
   console.log('Connected to mongodb server');
 
-  db.collection('Todos').find(//{
-    //_id: new ObjectId('5b18d34ea9d37433ccc4a805')
-  //}
-).toArray().then((docs) => {
-
-    console.log(JSON.stringify(docs, undefined , 2));
-  }, (err) => {
-    console.log('Unable to find todos');
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectId('5b18d3a562d0a633ed435292')
+  }, {
+    $set: {
+      completed: false
+    }
+  },
+  {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
   });
 
   client.close();
