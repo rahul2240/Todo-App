f9f0292412286a3d06ec342543efea4db20932c0
Rahul <rahulsingh2240@gmail.com>
2018-06-07 22:45:01 +0530

    testing get todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const {Todo} = require('./../models/todo');
 const {app} = require('./../server');
 
+const todos = [{
+      text: 'text 1'
+    },{
+      text: 'text 2'
+    }];
+
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +33,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -44,10 +53,22 @@ describe('POST /todos', () => {
           return done(err)
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
   });
 
 });
+
+  describe('GET /todos', () => {
+    it('should return all todos', (done) => {
+      request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done)
+    });
+  });
