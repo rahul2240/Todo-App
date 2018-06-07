959f67fbff7424259d2827c6bcc041ab3b37bcbd
Rahul <rahulsingh2240@gmail.com>
2018-06-07 17:37:06 +0530

    default and validators

--- server/server.js
+++ server/server.js
@@ -6,24 +6,29 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
   },
   complete: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 var newTodo = new Todo({
-  text: 'second todo',
   complete: true,
+  text: 'fourth todo',
   completedAt: 123
 });
 
 newTodo.save().then((doc) => {
-  console.log(doc);
+  console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
   console.log('Unable to connect to the database');
 });
