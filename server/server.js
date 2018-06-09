e4ce8ea341f497b80ff792d58d160581a593edd6
Rahul <rahulsingh2240@gmail.com>
2018-06-09 12:47:59 +0530

    DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -50,6 +50,25 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(400).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(400).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
