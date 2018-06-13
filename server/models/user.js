f5602f63a7b32074ff27bbdc08c2aae980c58a88
Rahul <rahulsingh2240@gmail.com>
2018-06-13 16:58:53 +0530

    json web token

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -30,6 +32,27 @@ var User = mongoose.model('User', {
   }]
 
 });
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth'
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+}
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+
+var User = mongoose.model('User', UserSchema);
 
 
 module.exports = {User}
