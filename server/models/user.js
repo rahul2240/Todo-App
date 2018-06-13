e97872528b5d24cd06da7ff78b47f0e1d859cb12
Rahul <rahulsingh2240@gmail.com>
2018-06-13 17:24:50 +0530

    authentication

--- server/models/user.js
+++ server/models/user.js
@@ -43,14 +43,31 @@ UserSchema.methods.generateAuthToken = function () {
   return user.save().then(() => {
     return token;
   });
-}
+};
 
 UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
     return _.pick(userObject, ['_id', 'email']);
-}
+};
 
+UserSchema.statics.findByToken = function(token) {
+
+    var User = this;
+    var decoded;
+
+    try {
+      decoded = jwt.verify(token, 'abc123');
+    } catch(e) {
+      return Promise.reject();
+    }
+
+    return User.findOne({
+      '_id': decoded._id,
+      'tokens.token': token,
+      'tokens.access': 'auth'
+    });
+};
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
+
 var app = express();
 
 const port = process.env.port || 3000;
@@ -112,6 +114,14 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+
+  res.send(req.user);
+
 });
 
 app.listen(port, () => {
