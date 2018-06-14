321868330816e3dc443cc44c35a27a0304b32c62
Rahul <rahulsingh2240@gmail.com>
2018-06-14 13:09:46 +0530

    hashing password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -68,6 +69,21 @@ UserSchema.statics.findByToken = function(token) {
       'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next)  {
+  var user = this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+
+});
 
 var User = mongoose.model('User', UserSchema);
 
