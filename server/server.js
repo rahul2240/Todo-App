af19768a99c5226fa071074f9d036362a4dc684d
Rahul <rahulsingh2240@gmail.com>
2018-06-15 16:09:05 +0530

    POST /users/login

--- server/server.js
+++ server/server.js
@@ -124,6 +124,21 @@ app.get('/users/me', authenticate, (req, res) => {
 
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredential(body.email, body.password).then((user) => {
+
+    return user.generateAuthToken().then((token)=>{
+      res.header('x-auth', token).send(user);
+    });
+    
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
