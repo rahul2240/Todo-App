1cdfaa3d8951fa8c9cee34d4ca72da1bf5c91ff7
Rahul <rahulsingh2240@gmail.com>
2018-06-16 15:18:56 +0530

    configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,19 @@
 const env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envconfig = config[env];
 
-} else if(env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envconfig).forEach((key) => {
+    process.env[key] = envconfig[key];
+  });
 }
+
+// if(env === 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//
+// } else if(env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ var UserSchema = new mongoose.Schema({
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth'
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWB_SECRET).toString();
 
   user.tokens.push({access, token});
 
@@ -68,7 +68,7 @@ UserSchema.statics.findByToken = function(token) {
     var decoded;
 
     try {
-      decoded = jwt.verify(token, 'abc123');
+      decoded = jwt.verify(token, process.env.JWB_SECRET);
     } catch(e) {
       return Promise.reject();
     }
