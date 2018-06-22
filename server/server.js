b4f1483565dfa4111fcfba6b801fefcf894a8c44
Rahul <rahulsingh2240@gmail.com>
2018-06-22 23:22:41 +0530

    header

--- server/server.js
+++ server/server.js
@@ -11,6 +11,26 @@ var app = express();
 
 const port = 8080;
 
+app.use(function (req, res, next) {
+
+     // Website you wish to allow to connect
+     res.setHeader('Access-Control-Allow-Origin', '*');
+-     res.setHeader('x-auth', '*');
++     res.setHeader('Access-Control-Expose-Headers', 'x-auth');
+
+     // Request methods you wish to allow
+     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+     // Request headers you wish to allow
+     res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept');
+ 
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     // res.setHeader('Access-Control-Allow-Credentials', true);
+
+     // Pass to next layer of middleware
+     next();
+});
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
